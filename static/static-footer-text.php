54697ea37f06e9c79dc72fe5dbfa8ab9caa1ca97
Jonny Reeves <github@jonnyreeves.co.uk>
2018-05-14 21:56:16 +0100

    Tweak footer layout

--- static/static-footer-text.php
+++ static/static-footer-text.php
@@ -1,7 +1,8 @@
 <?php /* Static Name: Footer text */ ?>
 <div id="footer-text" class="footer-text">
 	<?php $hs_footer_text = of_get_option('footer_text'); ?>
-	<a href="https://hotpinkwellingtons.co.uk/privacy-policy/">Privacy Policy</a> | Header design by <a href="http://www.freepik.com">Freepik</a>. Content copyright <?php echo date("Y") ?>.</a>
+	<a href="https://hotpinkwellingtons.co.uk/privacy-policy/">Privacy Policy</a> | 
+	Header design by <a href="http://www.freepik.com">Freepik</a> |  Content copyright <?php echo date("Y") ?></a>
 
 	<?php if( is_front_page() ) { ?>
 		<!-- {%FOOTER_LINK} -->
