d0dcdb4b4d5b08d26086cb52509bb7d22a9d8545
Jonny Reeves <github@jonnyreeves.co.uk>
2018-05-14 21:51:27 +0100

    Add link to privacy policy

--- static/static-footer-text.php
+++ static/static-footer-text.php
@@ -1,7 +1,7 @@
 <?php /* Static Name: Footer text */ ?>
 <div id="footer-text" class="footer-text">
 	<?php $hs_footer_text = of_get_option('footer_text'); ?>
-	Header design by <a href="http://www.freepik.com">Freepik</a>. Content copyright <?php echo date("Y") ?>.</a>
+	<a href="https://hotpinkwellingtons.co.uk/privacy-policy/">Privacy Policy</a> | Header design by <a href="http://www.freepik.com">Freepik</a>. Content copyright <?php echo date("Y") ?>.</a>
 
 	<?php if( is_front_page() ) { ?>
 		<!-- {%FOOTER_LINK} -->
